de428972f755130693600f3b070b0a40ca8d8f8a
breslavsky <breslavsky.anton@gmail.com>
2022-02-28 16:56:47 +0100

    Fix base uri & couchdb path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ const COUCH_DB_URL_KEY = 'couch_db_url';
 export const environment = {
   production: true,
   mocks: false,
-  storage: localStorage.getItem(COUCH_DB_URL_KEY)  || '/couchdb',
+  storage: localStorage.getItem(COUCH_DB_URL_KEY) || [location.origin, 'couchdb'].join('/'),
   assets: '/media',
   graphql: '/api/graphql'
 };
